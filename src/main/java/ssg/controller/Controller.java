722f4b09cd14730aaeb265dab426c9b43c37736d
dpals-103 <dpals103@gmail.com>
2020-12-09 02:03:07 +0900

    Apply.gitignore

--- src/main/java/ssg/controller/Controller.java
+++
@@ -1,7 +0,0 @@
-package ssg.controller;
-
-public abstract class Controller {
-
-	abstract public void doAction(String cmd); 
-
-}
--- src/main/java/ssg/service/MemberService.java
+++
@@ -1,5 +0,0 @@
-package ssg.service;
-
-public class MemberService {
-
-}
